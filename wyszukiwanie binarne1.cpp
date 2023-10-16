500f10282b2ee94d06e4f55fa020a4b51f3f3fe0
Emilia Kendzia (Uczeń) <ekendzia360@technikumkreatywne.pl>
2023-10-16 14:11:50 +0200

    potegowanie szybkie1

--- wyszukiwanie binarne1.cpp
+++ wyszukiwanie binarne1.cpp
@@ -7,16 +7,22 @@ int main(){
 
 
 int l = 0;
-int p = 7;
+int p = 15;
 int sr = (l+p)/2;
 
+
+cout << "Podaj liczbe ktora chcesz znalezc: ";
 string text;
 cin >> text;
 int szukana=stoi(text);
 
+int tab[15] = {2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47};
+
 while (l <= p) {
+   // cout << l << " " << p << endl;
     if (tab[sr] == szukana){
-        cout << sr;
+        cout << sr << endl;
+        break;
     }
     if (tab[sr] > szukana){
         p= sr-1;
@@ -24,8 +30,16 @@ while (l <= p) {
     else {
         l= sr+1;
     }
-
+	
+    sr=(l+p)/2;
     
     }
+    if (tab[sr] == szukana){
+		cout << "Ta liczba wystepuje w zbiorze w komorce o indeksie" + sr;
+	}else{
+		cout << "Nie ma takiej liczby :(";
+	}
+	
+
     return -1;
 }
