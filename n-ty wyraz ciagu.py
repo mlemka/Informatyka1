6c40e3665373547ca00b6aef7a043326a79f2c5b
Emilia Kendzia (Uczeń) <ekendzia360@technikumkreatywne.pl>
2023-11-27 14:05:19 +0100

    n-ty wyraz ciagu

--- n-ty wyraz ciagu.py
+++ n-ty wyraz ciagu.py
@@ -1,10 +1,10 @@
 n=int(input('Podaj nr wyrazu ciagu, ktorego wartosc chcesz policzyc:'))
 
-def zbior(n):
-    if(n==1):
+def zbior(n):   #tworzymy funkcję
+    if(n==1):   #jeżeli n jest rowne jeden to zwroc 1 bo 
         return 1
-    elif(n==2):
+    elif(n==2): #jeżeli n jest rowne jeden to zwroc 0.5
         return 0.5
-    return zbior(n-1)*zbior(n-2)
+    return -zbior(n-1)*zbior(n-2)
 
 print(n,'wyraz ciagu ma wartosc:', zbior(n))
